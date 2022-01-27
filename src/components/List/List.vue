ffcb0754cd7cc3e4c0f458aa96bd115edecc4d42
viniciusbarth <viniciusbarthhh@hotmail.com>
2022-01-26 23:48:05 -0300

    adaptando para meu cenário

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-list flat>
+  <v-list flat class="dev-list">
     <v-list-item-group v-model="selectedItem" color="primary">
-      <v-list-item v-for="(item, i) in items" :key="i">
+      <v-list-item
+        class="dev-list__list-item"
+        v-for="(item, i) in items"
+        :key="i"
+      >
         <v-list-item-icon>
-          <v-icon v-text="item.icon"></v-icon>
+          <v-icon>{{ getIcon(item.step) }}</v-icon>
         </v-list-item-icon>
         <v-list-item-content>
-          <v-list-item-title v-text="item.text"></v-list-item-title>
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
         </v-list-item-content>
+        <v-list-item-action>
+          <p>teste</p>
+        </v-list-item-action>
       </v-list-item>
     </v-list-item-group>
   </v-list>
@@ -16,13 +23,34 @@
 export default {
   data() {
     return {
-      selectedItem: 1,
+      selectedItem: 0,
       items: [
-        { text: "Real-Time", icon: "mdi-clock" },
-        { text: "Audience", icon: "mdi-account" },
-        { text: "Conversions", icon: "mdi-flag" },
+        //TODO step será as etapas em que os projetos se encontram 0 = em andamento, 1 = finalizado, 2 = pausado
+        { text: "Projeto tech blog", step: 0 },
+        { text: "Projeto software para automóveis ", step: 1 },
+        { text: "software para a área pet", step: 2 },
       ],
     };
   },
+  computed: {},
+  methods: {
+    getIcon(icon) {
+      const icons = {
+        0: "mdi-clock-time-five",
+        1: "mdi-check",
+        2: "mdi-pause-circle",
+      };
+      return icons[icon];
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+.dev-list {
+  background-color: transparent;
+  &__list-item {
+    background-color: white;
+    margin-top: 8px;
+  }
+}
+</style>
