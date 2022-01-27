4d65679fa0d8f0a9455e458b6d0ff1f8eea030ae
viniciusbarth <viniciusbarthhh@hotmail.com>
2022-01-26 23:11:48 -0300

    Adição de lista na página colaborate

--- src/pages/Colaborate/Colaborate.vue
+++ src/pages/Colaborate/Colaborate.vue
@@ -4,14 +4,16 @@
       <v-row no-gutters class="d-flex justify-space-between mt-10 mb-6">
         <h1 class="page-title">Projetos que participo</h1>
       </v-row>
+      <List></List>
     </div>
   </v-container>
 </template>
 
 <script>
+import List from "../../components/List/List.vue";
 export default {
   name: "Colaborate",
-  components: {},
+  components: { List },
   data() {
     return {};
   },
